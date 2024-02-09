b540cce52d65b45371dab05fb2a317c7ee58c768
n-tsuki <155265037+n-tsuki@users.noreply.github.com>
2024-02-09 06:55:50 +0000

    first-commit

---
+++ backend/app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Reporter(models.Model):
+    full_name = models.CharField(max_length=70)
+
+    def __str__(self):
+        return self.full_name
